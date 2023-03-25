644e8dc03fb26008a7267cfb3896a552c87f4f96
Ryan Le <ryanle@live.com>
2023-03-25 12:38:17 -0400

    fix: env vars for contentful

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    BASE_URL: process.env.BASE_URL,
+    CONTENTFUL_BASE_URL: process.env.NEXT_PUBLIC_CONTENTFUL_BASE_URL,
+    CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
   },
   images: {
     remotePatterns: [
