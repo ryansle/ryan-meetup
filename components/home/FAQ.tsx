be18bc6c7400e700d6d286edd8fcb441fcb1accd
Ryan Le <ryanle@live.com>
2023-03-27 22:19:37 -0400

    feat: loading skeletons for faq

--- components/home/FAQ.tsx
+++ components/home/FAQ.tsx
@@ -19,7 +19,18 @@ const FAQ = (props: FAQProps) => {
 
       {/* TODO: use disclosure component */}
       <div>
-        {data && data?.map((pair, index) => (
+        {isLoading && (
+          <>
+            {Array(5).fill('').map((_, index) => (
+              <div key={index} className='mb-10'>
+                <div className='h-4 bg-gray-700 rounded-full w-48 mb-4 animate-pulse' />
+                <div className='h-3 bg-gray-700 rounded-full max-w-[360px] mb-2.5 animate-pulse' />
+              </div>
+            ))}
+          </>
+        )}
+
+        {!isLoading && data && data?.map((pair, index) => (
           <div key={index}>
             <>
               <div className='text-xl font-semibold tracking-wider mb-2'>
