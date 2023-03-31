1e82b10d289aa5c4e398ecbcbeddb127e7a98be6
Ryan Le <ryanle@live.com>
2023-03-30 21:39:36 -0400

    feat: mobile responsive construction

--- components/UnderConstruction.tsx
+++ components/UnderConstruction.tsx
@@ -6,17 +6,43 @@ const UnderConstruction = () => {
   return (
     <div className='h-96 flex items-center justify-center flex-col text-center'>
       <Construction className='w-48 h-48' />
-      <Heading
-        className='mb-10'
-        variant='normal'
-        bold
-        size='2xl'
-      >
-        This page is under construction!
-      </Heading>
-      <Heading variant='normal' bold size='xl'>
-        Check back soon!
-      </Heading>
+
+      {/* Mobile */}
+      <div className='block sm:hidden'>
+        <Heading
+          className='mb-10'
+          variant='normal'
+          bold
+          size='lg'
+        >
+          This page is under construction!
+        </Heading>
+      </div>
+
+      {/* Desktop */}
+      <div className='hidden sm:block'>
+        <Heading
+          className='mb-10'
+          variant='normal'
+          bold
+          size='2xl'
+        >
+          This page is under construction!
+        </Heading>
+      </div>
+
+      <div className='block sm:hidden'>
+        <Heading variant='normal' bold size='md'>
+          Check back soon!
+        </Heading>
+      </div>
+
+      <div className='hidden sm:block'>
+        <Heading variant='normal' bold size='lg'>
+          Check back soon!
+        </Heading>
+      </div>
+
     </div>
   );
 };
