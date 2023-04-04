6c24c89fb349ea1fcb9dfd0ba43acc0ef8b33d30
Ryan Le <ryanle@live.com>
2023-04-03 20:08:55 -0400

    feat: zoom feature on gallery

--- app/gallery/[slug]/page.tsx
+++ app/gallery/[slug]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 
 // Components
 import { Layout } from '@/components/navigation';
@@ -6,9 +7,11 @@ import { Heading, Text } from '@/components/global';
 import { Photo } from '@/components/media/Photo';
 import PageNotFound from '@/components/404';
 import NextLink from 'next/link';
+import { BiZoomIn as ZoomIn, BiZoomOut as ZoomOut } from 'react-icons/bi';
 
 // Types
-import { RyanPhoto } from '@/lib/types';
+import type { RyanPhoto } from '@/lib/types';
+import type { ReactNode } from 'react';
 
 // Utilities
 import { useQuery } from 'react-query';
@@ -20,13 +23,40 @@ type GalleryPageProps = {
   }
 }
 
+type ZoomButtonProps = {
+  children: ReactNode;
+  onClick: () => void;
+  rightIcon: ReactNode;
+  disabled: boolean;
+}
+
+const ZoomButton = (props: ZoomButtonProps) => {
+  const { children, onClick, rightIcon, disabled } = props;
+
+  return (
+    <button
+      className='rounded-lg p-3 bg-black uppercase text-neutral-400 font-semibold border border-gray-700 transition duration-300 ease-in hover:text-white disabled:text-gray-700'
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <div className='flex items-center justify-center'>
+        {children} <span className='ml-2'>{rightIcon}</span>
+      </div>
+    </button>
+  );
+};
+
 // TODO: look into ssr approaches for fetching this data
-// TODO: 404 for junk slugs
 const GalleryPage = ({ params }: GalleryPageProps) => {
   const { data, isLoading, isError } = useQuery('gallery', () => fetchSingleMediaEvent(params.slug));
 
+  const [zoom, setZoom] = useState<number>(3);
+
+  console.log(zoom);
+
   return (
     <Layout>
+      {/* Render 404 contents if user inputs a junk slug */}
       {isError && (
         <PageNotFound>
           <Text className='text-center' size='lg'>
@@ -35,18 +65,54 @@ const GalleryPage = ({ params }: GalleryPageProps) => {
         </PageNotFound>
       )}
 
-      {/* TODO: loading header */}
-      {!isLoading && (
-        <Heading className='mb-4'>
-          {/* @ts-ignore */}
-          {data?.title}
-        </Heading>
-      )}
-
-      <div className='grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-2 xl:grid-cols-3'>
+      <div className='flex justify-between'>
         <>
           {isLoading && (
-            Array(4).fill('').map((_, index) => (
+            <div className='animate-pulse bg-gray-700 rounded-3xl w-[344px] h-10 mb-10' />
+          )}
+
+          {/* TODO: loading header */}
+          {!isLoading && (
+            <Heading className='mb-10'>
+              {/* @ts-ignore */}
+              {data?.title}
+            </Heading>
+          )}
+        </>
+
+        <div className='hidden md:block md:space-x-4'>
+          {isLoading && (
+            <div className='space-x-4 flex'>
+              <div className='animate-pulse bg-gray-700 rounded-lg w-36 h-10 mb-10' />
+              <div className='animate-pulse bg-gray-700 rounded-lg w-36 h-10 mb-10' />
+            </div>
+          )}
+
+          {!isLoading && (
+            <>
+              <ZoomButton
+                rightIcon={<ZoomOut />}
+                onClick={() => setZoom(zoom + 1)}
+                disabled={zoom >= 3}
+              >
+                Zoom Out
+              </ZoomButton>
+              <ZoomButton
+                rightIcon={<ZoomIn />}
+                onClick={() => setZoom(zoom - 1)}
+                disabled={zoom <= 1}
+              >
+                Zoom In
+              </ZoomButton>
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className={`grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-${zoom} xl:grid-cols-${zoom}`}>
+        <>
+          {isLoading && (
+            Array(6).fill('').map((_, index) => (
               <div
                 key={index}
                 className='animate-pulse bg-gray-700 rounded-3xl w-full h-full max-h-[450px] aspect-w-2 aspect-h-1'
