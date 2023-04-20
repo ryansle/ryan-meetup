0f8bd4eb4968d8b352b7e8bcac92a42c5afc267b
Ryan Le <ryanle@live.com>
2023-04-20 13:39:51 -0400

    feat: add 404, seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+// Components
+import Head from 'next/head';
+
 // Types
 import type { AppProps } from 'next/app';
 
@@ -10,6 +13,20 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Ryan Meetup</title>
+        <meta
+          name='description'
+          content='Wanna meet other Ryans? Join the Ryan Meetup!'
+        />
+        <meta property='og:image' content='/ryanroundup.png' />
+        <meta
+          name='keywords'
+          content='ryan, ryan meetup, meetup, ryan society, no bryans allowed, is your name ryan, join the ryan meetup, wanna meet other ryans, ryan meetup posters, first name must be ryan, ryanmeetup, ryan meet up'
+        />
+        <meta name='author' content='Ryan Meetup' />
+        <link rel='shortcut icon' href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </QueryClientProvider>
   );
