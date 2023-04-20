a4ee48fbbaba1c2302596d94f3c684a7e417d5f7
Ryan Le <ryanle@live.com>
2023-04-20 15:29:31 -0400

    fix: hamburger for MobileMenu

--- components/navigation/Layout.tsx
+++ components/navigation/Layout.tsx
@@ -1,3 +1,5 @@
+import { Header } from './Header';
+
 // Types
 import type { ReactNode } from 'react';
 
@@ -10,9 +12,11 @@ const Layout = (props: LayoutProps) => {
   const { className, children } = props;
 
   return (
-    <main className={`${className} text-white h-full py-8 px-4 flex flex-col overflow-hidden bg-gradient-to-b from-neutral-900 to-black lg:px-48 2xl:px-96 3xl:px-[400px] 4xl:px-[650px]`}>
-
-      {children}
+    <main>
+      <Header />
+      <section className={`${className} text-white h-full py-8 px-4 flex flex-col overflow-hidden bg-gradient-to-b from-neutral-900 to-black lg:px-48 2xl:px-96 3xl:px-[400px] 4xl:px-[650px]`}>
+        {children}
+      </section>
     </main>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <Header />
+
         <Main />
         <Footer />
         <NextScript />
