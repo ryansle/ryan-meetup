0ab707d89ae52917701d45808872d0761812279b
Ryan Le <ryanle@live.com>
2023-08-04 22:31:36 -0400

    copy: adjust copy text for landing page

--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -5,13 +5,13 @@ const Banner = () => {
   return (
     <div className='font-cooper bg-red-400 py-1 flex justify-between grid grid-cols-12 px-2 lg:px-32 xl:px-72 3xl:px-[400px] 4xl:px-[650px]'>
       <p className='col-span-12 xl:col-span-6'>
-        Ryan Meetup Merch available now!
+        New Ryan Meetup merch available now!
       </p>
-      <p className='col-span-12 mt-4 xl:col-span-6 xl:mt-0'>
-        Support the Ryan Meetup by checking out our{' '}
-        <NextLink className='text-red-800 hover:underline' href='/merch'>/merch</NextLink> page!
+      <p className='flex justify-end col-span-12 mt-4 xl:col-span-6 xl:mt-0'>
+        Support the Ryan Meetup by checking out our&nbsp;
+        <NextLink className='text-red-800 hover:underline' href='/merch'>/merch</NextLink>&nbsp; page!
       </p>
-    </div >
+    </div>
   );
 };
 
