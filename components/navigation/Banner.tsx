9160fe6621f2533a31d865a0d02b2f8df7202b5d
Ryan Le <ryanle@live.com>
2023-08-24 17:21:28 -0400

    feat: update banner with rave announcement

--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className='font-cooper bg-red-400 py-1 px-2 lg:px-32 xl:px-72 3xl:px-[400px] 4xl:px-[650px]'>
       <p>
-        New Ryan Meetup merch available now on our <NextLink className='text-red-800 hover:underline' href='/merch'>/merch</NextLink>{' '} page!
+        We just dropped the location for the RYAN RAVE! Details on our <NextLink className='text-red-800 hover:underline' href='/events'>/events</NextLink>{' '} page!
       </p>
     </div>
   );
