eeea56015edd4918b258de4141e7d3a500421fd1
Ryan Le <ryanle@live.com>
2023-09-03 15:04:01 -0700

    feat: update banner to ryan rave shirts

--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className='font-cooper bg-red-400 py-1 px-2 lg:px-32 xl:px-72 3xl:px-[400px] 4xl:px-[650px]'>
       <p>
-        We just dropped the location for the RYAN RAVE! Details on our <NextLink className='text-red-800 hover:underline' href='/events'>/events</NextLink>{' '} page!
+        Ryan Rave shirts still available on our <NextLink className='text-red-800 hover:underline' href='/merch'>/merch</NextLink>{' '} page!
       </p>
     </div>
   );
