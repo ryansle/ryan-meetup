da77e68788cdc6c5789717aaf6e831e951bfd4ca
Ryan Le <ryanle@live.com>
2024-01-10 15:26:41 -0500

    fix: local storage to remember light mode selection

--- components/global/ThemeSwitcher.tsx
+++ components/global/ThemeSwitcher.tsx
@@ -1,26 +1,13 @@
-import { useState, useEffect } from 'react';
-
 // Components
 import { FiSun as SunIcon } from 'react-icons/fi';
 import { FaMoon as MoonIcon } from 'react-icons/fa';
 
+// Utilities
+import useColorMode from '@/hooks/useColorMode';
+
 
 const ThemeSwitcher = () => {
-  const [darkMode, setDarkMode] = useState(true);
-
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    setDarkMode(isDarkMode);
-  }, []);
-
-  useEffect(() => {
-    document.documentElement.classList.toggle('dark', darkMode);
-    localStorage.setItem('darkMode', darkMode.toString());
-  }, [darkMode]);
-
-  const toggleDarkMode = () => {
-    setDarkMode((prevMode) => !prevMode);
-  };
+  const [darkMode, toggleDarkMode] = useColorMode(true);
 
   const classes = 'fill-black dark:fill-white w-6 h-6';
 
---
+++ hooks/useColorMode.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const useColorMode = (initialValue: boolean) => {
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const storedValue = localStorage?.getItem('darkMode');
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    }
+  });
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    setDarkMode(isDarkMode);
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', darkMode);
+    localStorage.setItem('darkMode', darkMode.toString());
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode: boolean) => !prevMode);
+  };
+
+  return [darkMode, toggleDarkMode];
+};
+
+export default useColorMode;
