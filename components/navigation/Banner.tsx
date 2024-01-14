0c9287681b27a36cf68a5e1fcb97ba619b93a37c
Ryan Le <ryanle@live.com>
2024-01-14 12:58:55 -0500

    chore: update banner copy

--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -4,10 +4,7 @@ import NextLink from 'next/link';
 const Banner = () => {
   return (
     <div className='font-cooper text-white bg-red-400 py-1 px-2 lg:px-32 xl:px-72 3xl:px-[400px] 4xl:px-[650px]'>
-      <p>
-        Get ready for a rootin&apos; tootin&apos; time, Ryans. RSVP to the{' '}
-        <NextLink href='/rsvp' className='text-red-900 hover:underline'>Ryan Rodeo</NextLink> today.
-      </p>
+      <NextLink href='/rsvp' className='hover:underline'>Ryan Rodeo. Austin, TX. Feb 24</NextLink>
     </div>
   );
 };
