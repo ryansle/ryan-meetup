289dc3b6d14d3662c51be943d37e000762fe36fa
Ryan Le <ryanle@live.com>
2024-02-28 19:20:30 -0500

    copy: banner

--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -4,7 +4,7 @@ import NextLink from 'next/link';
 const Banner = () => {
   return (
     <div className='font-cooper text-white bg-red-400 py-1 px-2 lg:px-32 xl:px-72 3xl:px-[400px] 4xl:px-[650px]'>
-      <NextLink href='/rsvp' className='hover:underline'>Ryan Rodeo. Austin, TX. Feb 24</NextLink>
+      Forecasting another Ryan Meetup coming soon. Expect windy conditions as a major Ryan front moves in.
     </div>
   );
 };
